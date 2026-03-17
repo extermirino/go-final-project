52d9833a70d9c809e170c7ecd709d79bf46a3680
extermirino <daniyal.osmanov@gmail.com>
2026-03-17 12:37:38 +0300

    add database initialization with TODO_DBFILE support

--- main.go
+++ main.go
@@ -2,11 +2,24 @@ package main
 
 import (
 	"fmt"
+	"go1f/pkg/db"
 	"go1f/pkg/server"
+	"os"
 )
 
 func main() {
-	err := server.Run()
+	dbFile := os.Getenv("TODO_DBFILE")
+	if dbFile == "" {
+		dbFile = "scheduler.db"
+	}
+
+	err := db.Init(dbFile)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	err = server.Run()
 	if err != nil {
 		fmt.Println(err)
 		return
